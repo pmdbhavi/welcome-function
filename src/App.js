78bc5ee6ca6201176409a249683510bd82e5a84a
pmdbhavi <srirenu640@gmail.com>
2023-03-24 21:13:19 +0530

    welcome-function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Container,Con,Heading,Para,Button} from "./styledComponents"
+
+const App=()=>{
+  const [subscribe,setSubscribe]=useState(false)
+
+  const onClickButton=()=>{
+    setSubscribe(prevState=>(!prevState))
+  }
+
+  return(
+      <Container>
+        <Con>
+          <Heading>Welcome</Heading>
+          <Para>Thank you! Happy Learning</Para>
+          <Button onClick={onClickButton}>{subscribe ? "Subscribed": "Subscribe"}</Button>
+        </Con>
+      </Container>
+  )
 }
 
-export default App;
+export default App
---
+++ src/styledComponents.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container=styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color:#1e293b;
+    background-size:cover;
+`
+export const Con=styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Heading=styled.h1`
+    color:#f0bb03;
+    font-family:"Bree Serif";
+    font-weight:bold;
+    font-size:50px;
+`
+
+export const Para=styled.p`
+    color:white;
+    font-family:'Bree Serif';
+`
+
+export const Button=styled.button`
+    background-color:white;
+    color:black;
+    font-family:'sans serif';
+    padding:10px;
+    border-width:0px;
+    border-radius: 10px;
+    cursor: pointer;
+    font-family:serif;
+`
+
